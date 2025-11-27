f51b7e84e5de56be366444aa94a4753b11de24e2
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-11-26 20:42:30 -0800

    exersice is done

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-$(function() {
-  // your code here
+$(function () {
+  $('.accordion .accordion-header').on('click', function () {
+    $(this).next('.accordion-content').slideToggle()
+  })
+
+  function buildTodos(todos) {
+    let list = ""
+    for (let i = 0; i < todos.length; i++) {
+      list += "<li>" + todos[i].todo + "</li>"
+    }
+    $('.todos ul').html(list)
+  }
+
+  $('.todos button').on('click', function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function (data) {
+        buildTodos(data.todos)
+      },
+      error: function (err) {
+        console.error(err)
+      }
+    })
+  })
 })
